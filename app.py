7cecbda189bcc65e9c737259c774d4f33cb99558
Sofia Quitian <you@example.com>
2026-09-05 11:08:51 -0500

    Add use cases 1 and 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-if __name__ == "__main__":
+@app.route("/use-case-1")
+def use_case_1():
+    return render_template("use_case_1.html")
 
+@app.route("/use-case-2")
+def use_case_2():
+    return render_template("use_case_2.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
