580babc3661e03f9265b47c43e8512564f8f38b7
Sofia Quitian <you@example.com>
2026-09-05 10:29:09 -0500

    Add navigation menu, and modifiqued home.html

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def pg():
+def home():
     return render_template("home.html")
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
