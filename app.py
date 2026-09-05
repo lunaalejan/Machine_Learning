e52a3ad704c4b8c03c0362c0412254a411297f71
lunaalejan <lunaalejanmm13@gmail.com>
2026-09-05 11:50:48 -0500

    use case 3 and 4

--- app.py
+++ app.py
@@ -14,5 +14,13 @@ def use_case_1():
 def use_case_2():
     return render_template("use_case_2.html")
 
+@app.route("/use-case-3")
+def use_case_3():
+    return render_template("use_case_3.html")
+
+@app.route("/use-case-4")
+def use_case_4():
+    return render_template("use_case_4.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
